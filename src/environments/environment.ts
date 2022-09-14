1930f716d2d59a960577e4398421a3cf3a9ed421
paolaveliez <graficaviamonte@yahoo.com.ar>
2022-09-14 20:22:12 -0300

    actualizado

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,8 +2,12 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+import { URL } from "url";
+
 export const environment = {
-  production: false
+  production: false,
+
+  URL:'https://portfoliopao.herokuapp.com/'
 };
 
 /*
